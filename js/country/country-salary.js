41270635e1e9bfad332a08967f08625a94039dd8
Alara-Yoga <alaratin@gmail.com>
2024-07-27 06:21:01 +0300

    legends done

--- js/country/country-salary.js
+++ js/country/country-salary.js
@@ -1,6 +1,7 @@
 // "use strict"
 
 async function init() {
+
   // @ Data Selection
   // const data = await d3.csv("https://raw.githubusercontent.com/alaratin/cs416-atin4.github.io/main/data/ds_salaries.csv");
   window.data = await d3.csv("https://raw.githubusercontent.com/JorgeMiGo/Data-Science-Salaries-2023/main/Dataset/ds_salaries.csv");
@@ -11,7 +12,7 @@ async function init() {
 // =========================================== DATA SELECTION & GROUPING =========================================
 // ===============================================================================================================
 function getData(data, index=0){
-    console.log("here")
+
     const year_arr = ['2020','2021', '2022', '2023']
     const year_val = year_arr[index]
     console.log(year_val)
@@ -136,34 +137,55 @@ function getData(data, index=0){
 // =================================================================================================================
 // ============================================== MAP COLORING =====================================================
 // =================================================================================================================
-const color = d3.scaleSequential(d3.interpolateWarm)
+const color = d3.scaleSequential(d3.interpolateViridis)
       .domain([0, d3.max(grouped_data_SE, d => d.mean_salary)]);
 
 const countryDataMap = new Map(grouped_data_SE.map(d => [d.country_loc, d.mean_salary]));
+console.log("COUNTRY",countryDataMap)
 
-// ===========================================
-// @ Legend definition for salary allocations
-var legend = d3.legendColor()
-    .scale(color);
-// ===========================================
   if(!window.draw_flag){
     drawCanvas(countryDataMap,path,color, index)
     setTimeout(() => {getData(data, index + 1)},5000);
   }
   else{
+
     d3.select("svg").selectAll("path")
       .style("fill", d => {
         const salary = countryDataMap.get(d.properties.name) || 0;
-        return salary>0 ? color(salary) : "#000";    
+        return salary>0 ? color(salary) : "#818589";    
       });
+      
+      const countryDataMap_filtered = new Map(Array.from(countryDataMap).filter
+      (([key,value]) => value > 0).sort((a,b) => d3.descending(a[1], b[1])));
+      console.log("COUNTRY-VERY",countryDataMap_filtered)
+
+
+      d3.select("svg").selectAll(".legend").select("rect")
+        .data(countryDataMap_filtered)
+        .style("fill", ([name,value]) => {
+          const salary = countryDataMap_filtered.get(name);
+            return salary>0 ? color(salary) : "#000"
+        });
+      
+        d3.select("svg").selectAll(".legend").select("text")
+        .data(countryDataMap_filtered)
+
+        .text(([name, value]) => "$" + countryDataMap_filtered.get(name).toFixed(2));
+      
+
+    
     setTimeout(() => {
         if (index == 3){
           index = -1;
           getData(data, index + 1);
         }
-        else{ getData(data, index + 1);}
+        else{ 
+          getData(data, index + 1);
+        }
       }, 5000)
   }
+
+
 }
 
 // ===============================================================================================================
@@ -181,7 +203,7 @@ function drawCanvas(countryDataMap, path, color, index){
             .attr("class", "country")
             .style("fill", d => {
               const salary = countryDataMap.get(d.properties.name) || 0;
-              return salary>0 ? color(salary) : "#000";
+              return salary>0 ? color(salary) : "#818589";
             });
   
     d3.select('svg')
@@ -191,21 +213,46 @@ function drawCanvas(countryDataMap, path, color, index){
           const count = countryDataMap.get(d.properties.name) || 0;
           return `Mean Salary for Data Scientist Job in ${d.properties.name}: $ ${count}`;
         });
-    
-  // d3.select('svg')
-  //     .append("g")
-  //     .attr("transform", "translate(10,10)")
-  //     .call(legend);
+     
+  var width = 500;
+  var height = 500;
+
+  const countryDataMap_filtered = new Map(Array.from(countryDataMap).filter
+  (([key,value]) => value > 0).sort((a,b) => d3.descending(a[1], b[1])));
+
+  const legend = d3.select('svg').selectAll(".legend")
+    .data(countryDataMap_filtered)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => "translate(15,"+(25*i)+")");
+
+  legend.append("rect")
+    .attr('x', width/2 - 150)
+    .attr('y', 220)
+    .attr("width", 12)
+    .attr("height", 12)
+    .style("fill", ([name,value]) => {
+      const salary = countryDataMap_filtered.get(name);
+        return salary>0 ? color(salary) : "#000"   
+    });
+
+  legend.append("text")
+    .attr("x", width/2 - 130)
+    .attr("y", 230)
+    .attr("dy", ".10em")
+    .style("text-anchor", "start")
+    .text(([name, value]) => "$" + countryDataMap_filtered.get(name).toFixed(2));
+
+
   });
 
  
   // ===================================================================================================================
-  // @ Clear svg for updading parameter
-  var svg = d3.select("svg");
-  svg.selectAll("*").remove();
+  // @ Clear svg for updating parameter
+  // var svg = d3.select("svg");
+  // svg.selectAll("*").remove();
   // ===================================================================================================================
 
   window.draw_flag = 'true';
-
 }
 
